59540563b2350de2ecd89d2f26becebd3396dc67
Gareth Nunns <garethnunns@gmail.com>
2021-05-16 15:42:32 +0100

    Base Receiver

---
+++ receiver.py
@@ -0,0 +1,24 @@
+import sacn
+import time
+import netifaces
+
+receiver = sacn.sACNreceiver()
+# start the receiving thread
+receiver.start()
+
+# define a callback function
+@receiver.listen_on('universe', universe=1)  # listens on universe 1
+def callback(packet):  # packet type: sacn.DataPacket
+  # print the received info
+  print(packet)
+
+# check for availability of universes
+@receiver.listen_on('availability')
+def callback_available(universe, changed):
+  print(f'universe {universe}: {changed}')
+
+receiver.join_multicast(1)
+
+# receive for a bit
+time.sleep(3)
+receiver.stop()
